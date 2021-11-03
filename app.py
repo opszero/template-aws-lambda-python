8fa866418885975720dee19f8ffb35d9e47c5611
Abhi Yerra <abhi@berkeley.edu>
2021-11-03 11:49:56 -0700

    Added dynamodb

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 from flask import Flask, render_template
+
+from pynamodb.models import Model
+from pynamodb.attributes import UnicodeAttribute
+
 app = Flask(__name__)
+
+class OneTable(Model):
+    '''
+    DynamoDB works best with a single table. This table. Use this table to model
+    other data requests.
+    '''
+    class Meta:
+        table_name = os.environ['DYNAMODB_TABLE_NAME']
+
+    pk = UnicodeAttribute(hash_key=True)
+    sk = UnicodeAttribute(range_key=True)
 
 @app.route("/")
 def cats():
