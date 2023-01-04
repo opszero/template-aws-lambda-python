697885fcd33122405f61bd15676a15cfa9df1f64
Thaung Htike Oo <zwe.zzzin@gmail.com>
2023-01-04 21:24:27 +0630

    added fastapi, mangum and updated app

--- app.py
+++ app.py
@@ -1,31 +1,36 @@
 import os
 
-from flask import Flask, render_template
-
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from mangum import Mangum
 from pynamodb.models import Model
 from pynamodb.attributes import UnicodeAttribute
 
-app = Flask(__name__)
 
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 class OneTable(Model):
     '''
     DynamoDB works best with a single table. This table. Use this table to model
     other data requests.
     '''
     class Meta:
-        table_name = os.environ['DYNAMODB_TABLE_NAME']
+        table_name = 'DYNAMODB_TABLE_NAME'
 
     pk = UnicodeAttribute(hash_key=True)
     sk = UnicodeAttribute(range_key=True)
 
-@app.route("/")
-def cats():
-    return render_template('index.html')
+@app.get("/", response_class=HTMLResponse)
+def cats(request: Request):
+    return templates.TemplateResponse("index.html", {"request":request})
 
-@app.route("/dogs/<id>")
+@app.get("/dogs/{id}")
 def dog(id):
     return "Dog"
 
-@app.route("/health")
+@app.get("/health")
 def health():
-    return { "status": "Success" }
+    return {"status": "Success"}
+
+handler = Mangum(app)
