765d13aea810588c9607e8c565e1de0a2753d834
Abhi Yerra <abhi@berkeley.edu>
2023-08-04 15:39:16 -0700

    Use postgresql

--- mrwork/settings.py
+++ mrwork/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,8 +81,12 @@ WSGI_APPLICATION = "mrwork.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("POSTGRES_NAME", "postgres"),
+        "USER": os.environ.get("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
+        "HOST": os.environ.get("POSTGRES_HOST", "0.0.0.0"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
